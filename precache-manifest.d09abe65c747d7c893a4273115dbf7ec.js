6356a48540f377f3d0e8cecf51665d83b37777b4
Jona Enzinger <uhock@student.kit.edu>
2020-07-04 15:15:09 +0200

    Updates

---
+++ precache-manifest.d09abe65c747d7c893a4273115dbf7ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1df3af42e6e1f211eaecf39180de0f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc95823f6097a51ce1c9",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "5cf4968f5fcc0fad1384",
+    "url": "/static/js/2.968220f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.968220f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc95823f6097a51ce1c9",
+    "url": "/static/js/main.4726a66b.chunk.js"
+  },
+  {
+    "revision": "11cde3ea961917c26c87",
+    "url": "/static/js/runtime-main.bf76a6d1.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
