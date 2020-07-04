98b29e0946d90eb250a0321ff325e7e264419e64
Jona Enzinger <uhock@student.kit.edu>
2020-07-04 16:11:16 +0200

    Updates

---
+++ precache-manifest.96088e1ac029bd742399ba76853e6062.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc362e536b583b1110f4eaffd770fdf3",
+    "url": "/pse-airquality-react/index.html"
+  },
+  {
+    "revision": "22d7b22f0c65ac04a151",
+    "url": "/pse-airquality-react/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "5cf4968f5fcc0fad1384",
+    "url": "/pse-airquality-react/static/js/2.968220f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pse-airquality-react/static/js/2.968220f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d7b22f0c65ac04a151",
+    "url": "/pse-airquality-react/static/js/main.6dd26d4a.chunk.js"
+  },
+  {
+    "revision": "98d2a021ed3822eec6f1",
+    "url": "/pse-airquality-react/static/js/runtime-main.56fb4d4d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/pse-airquality-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d09abe65c747d7c893a4273115dbf7ec.js"
+  "/pse-airquality-react/precache-manifest.96088e1ac029bd742399ba76853e6062.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pse-airquality-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
