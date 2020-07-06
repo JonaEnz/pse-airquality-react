20a8015a8f51a537cdd1f5afa0e7acaed453c445
ulfpr <ulfpr@student.kit.edu>
2020-07-06 22:29:38 +0200

    Add changes to LanguageMenu

--- src/View/LayoutComponents/LanguageMenu.tsx
+++ src/View/LayoutComponents/LanguageMenu.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Menu, Button } from '@material-ui/core';
+import { Menu, Button, MenuItem } from '@material-ui/core';
 import { Translate, ExpandMore } from '@material-ui/icons';
+import Language from '../../Controller/Storage/Language';
 
 interface IState {
     selectedLang: string;
-    menuOpen: boolean;
+    anchorEl: Element | null;
 }
 
 interface IProps {
@@ -13,22 +14,41 @@ interface IProps {
 
 export default class LanguageMenu extends React.Component<IProps, IState> {
 
+    private availableLanguages: string[];
+    private ref: any;
+
     constructor(props: IProps) {
         super(props)
         this.state = {
-            selectedLang: 'Deutsch',
-            menuOpen: false
+            selectedLang: Language.getSelectedLanguage(),
+            anchorEl: null
         }
+        this.handleClick = this.handleClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.availableLanguages = Language.getAvailabeleLanguages();
     }
 
-    render() {
+    handleClick(event: React.MouseEvent<HTMLButtonElement>): void {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose(): void {
+        this.setState({ anchorEl: null })
+    }
+
+    render(): JSX.Element {
+
         return (
             <div>
-                <Button color='inherit' startIcon={<Translate />} endIcon={<ExpandMore />} aria-controls="simple-menu" aria-haspopup="true">
+                <Button onClick={this.handleClick} color='inherit' startIcon={<Translate />} endIcon={<ExpandMore />} aria-controls="simple-menu" aria-haspopup="true">
                     {this.state.selectedLang}
                 </Button>
-                <Menu open={this.state.menuOpen}>
-
+                <Menu open={Boolean(this.state.anchorEl)} anchorEl={this.state.anchorEl}>
+                    {this.availableLanguages.map((lang, index) =>
+                        <MenuItem onClick={this.handleClose} >
+                            {lang}
+                        </MenuItem>
+                    )}
                 </Menu>
             </div>
         );
