56be9f7652c83c1b78b7bae3d11825cbb8b7e3b8
ulfpr <ulfpr@student.kit.edu>
2020-07-06 22:28:39 +0200

    Dummy Language

--- src/Controller/Storage/Language.ts
+++ src/Controller/Storage/Language.ts
@@ -1,6 +1,19 @@
-export class Language {
+export default class Language {
+
   static getText(id: string): string {
     //TODO: Implement real method
     return id;
+  }
+
+  static getSelectedLanguage(): string {
+    return "Deutsch";
+  }
+
+  static changeLanguage(language: string): void {
+
+  }
+
+  static getAvailabeleLanguages(): string[] {
+    return ['Deutsch', 'English', 'Espanol'];
   }
 }
