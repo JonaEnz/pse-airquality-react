f483d1740ab50a2448808773e9fbab2690cb5c6a
ulfpr <ulfpr@student.kit.edu>
2020-07-07 15:02:05 +0200

    Modify language.ts

--- src/Controller/Storage/Language.ts
+++ src/Controller/Storage/Language.ts
@@ -1,19 +1,53 @@
+import * as languageData from './languages.json';
+
+interface Lang {
+  id: string;
+  name: string;
+  strings: any;
+}
+
 export default class Language {
 
-  static getText(id: string): string {
-    //TODO: Implement real method
+  private static languageInstance: Language;
+
+  private selectedLangId: string;
+  private languages: Lang[];
+  private selectedLang: Lang;
+
+  constructor() {
+    this.selectedLangId = "de-de";
+    this.languages = languageData.languages;
+    this.selectedLang = this.languages[0];
+  }
+
+  public static getInstance(): Language {
+    if (this.languageInstance == null) {
+      this.languageInstance = new Language();
+    }
+    return this.languageInstance;
+  }
+
+  public getText(id: string): string {
+
     return id;
   }
 
-  static getSelectedLanguage(): string {
-    return "Deutsch";
+  public getSelectedLanguage(): string {
+    return this.selectedLang.name;
   }
 
-  static changeLanguage(language: string): void {
-
+  public changeLanguage(languageID: string): void {
+    let find: Lang | undefined = this.languages.find((e) => e.id == languageID);
+    if (find != undefined) {
+      this.selectedLang = find;
+    }
   }
 
-  static getAvailabeleLanguages(): string[] {
-    return ['Deutsch', 'English', 'Espanol'];
+  public getAvailabeleLanguages(): Map<string, string> {
+    let langs: Map<string, string> = new Map<string, string>();
+    this.languages.forEach(element => {
+      langs.set(element.id, element.name);
+    });
+    return langs;
   }
 }
