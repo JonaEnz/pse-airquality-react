1771120dc5edcf44e9622d49c9bea148191794d2
ufoeu <jonas.zoll@student.kit.edu>
2020-07-08 20:50:18 +0200

    Rework skeleton for DetailPage

--- src/View/DetailPage.tsx
+++ src/View/DetailPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import Fragment from 'react';
+import { Fragment } from 'react';
 import './DetailPage.css';
 import { Grid } from '@material-ui/core';
+import Hidden from '@material-ui/core/Hidden';
 
 import ObservationStationProfile from './ObservationStationProfile';
 import DataProvider from '../Controller/DataProvider'
 import { ObservationStation } from '../Model/ObservationStation';
+import Diagram from './Diagrams/Diagram';
+import LocationMap from './LocationMap';
 
 export default class DetailPage extends React.Component<IDetailPageProps, IDetailPageState> {
 
@@ -21,16 +24,25 @@ export default class DetailPage extends React.Component<IDetailPageProps, IDetai
 
     render() {
         return (
-            <Fragment>
-                <Grid container spacing={4}>
-                    <Grid item lg={3} sm={6} xl={3} xs={12} >
-                        <ObservationStationProfile observationStation={} />
+            <Grid container justify='center'>
+                <Grid className='main_container' container justify='center' spacing={4} xl={8} lg={8} md={8} sm={12} xs={12}>
+                    {/* every component on the page is wrapped in a Grid, which determines its size */}
+                    <Grid item xl={8} lg={8} md={8} sm={12} xs={12} >
+                        <ObservationStationProfile observationStation={this.observationStation} />
                     </Grid>
+                    <Hidden only={['sm', 'xs']}>
+                        <Grid item xl={4} lg={4} md={4}>
+                            <LocationMap position={this.observationStation.getPosition()} />
+                        </Grid>
+                    </Hidden>
+                    {this.observationStation.getDiagrams().map(diagram => diagram.render())}
                 </Grid>
-            </Fragment>
+                <Fragment>
+
+                </Fragment>
+            </Grid>
         );
     }
-
 }
 
 interface IDetailPageProps {
