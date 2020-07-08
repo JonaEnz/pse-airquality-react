41f66a95cf3350f8c0e3f2f642065bb87279c410
ufoeu <jonas.zoll@student.kit.edu>
2020-07-08 20:25:00 +0200

    Add skeleton for YearComparisonLineChart

---
+++ src/View/YearComparisonLineChart.tsx
@@ -0,0 +1,16 @@
+import Diagram from './Diagram';
+import { IDiagramProps } from './Diagram';
+
+export default class YearComparisonLineChart extends Diagram {
+
+    constructor(props: IDiagramProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>YearComparisonLineChart</h1>
+        );
+    }
+
+}
