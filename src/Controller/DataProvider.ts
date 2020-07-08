250a01e8daabe95bf31b78d1931cce84c76e3bd9
ufoeu <jonas.zoll@student.kit.edu>
2020-07-08 20:55:50 +0200

    Change diagram ids of mock data

--- src/Controller/DataProvider.ts
+++ src/Controller/DataProvider.ts
@@ -11,9 +11,9 @@ export default class DataProvider {
         var position = new Position(48.366216, 10.886720);
         var scale = new Scale(false, { 0: "#EEC000", 5: "#90B000", 10: "#FFFF00" });
         var features = [
-            new Feature('feat_1', 'feat_name_1', 'feat_desc_1', scale, 'feat_link_1', 60, 'feat_unit_1', ['1', '2', '3']),
-            new Feature('feat_2', 'feat_name_2', 'feat_desc_2', scale, 'feat_link_2', 90, 'feat_unit_2', ['1', '2']),
-            new Feature('feat_3', 'feat_name_3', 'feat_desc_3', scale, 'feat_link_3', 15, 'feat_unit_3', ['1'])
+            new Feature('feat_1', 'feat_name_1', 'feat_desc_1', scale, 'feat_link_1', 60, 'feat_unit_1', ['FeatureHistoryLineChart', 'ComparisonToLastYearPieChart', 'YearComparisonLineChart']),
+            new Feature('feat_2', 'feat_name_2', 'feat_desc_2', scale, 'feat_link_2', 90, 'feat_unit_2', ['FeatureHistoryLineChart', 'ComparisonToLastYearPieChart']),
+            new Feature('feat_3', 'feat_name_3', 'feat_desc_3', scale, 'feat_link_3', 15, 'feat_unit_3', ['FeatureHistoryLineChart'])
         ];
         var station = new ObservationStation(id, 'test_station', 'Dies ist eine Messstation, die zu Testzwecken existiert.', position, features);
         return station;
