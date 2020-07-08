4f19db70223e2312d7461c99b05bdc8ef17b07e0
ufoeu <jonas.zoll@student.kit.edu>
2020-07-08 20:26:30 +0200

    Add skeleton for LocationMap

---
+++ src/View/LocationMap.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card } from '@material-ui/core';
+
+import { Position } from '../Model/Position';
+
+export default class LocationMap extends React.Component<ILocationMapProps, ILocationMapState> {
+
+    constructor(props: ILocationMapProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Card>
+                <h1>LocationMap</h1>
+            </Card>
+        );
+    }
+}
+
+interface ILocationMapProps {
+    position: Position;
+}
+
+interface ILocationMapState { }
