55d2843741d6a50bcc21842798bafcacdd116c87
ufoeu <jonas.zoll@student.kit.edu>
2020-07-08 20:26:53 +0200

    Add skeleton for ObservationStationProfile

---
+++ src/View/ObservationStationProfile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './DetailPage.css';
+import {
+    Card,
+} from '@material-ui/core';
+
+import { ObservationStation } from '../Model/ObservationStation';
+
+export default class ObservationStationProfile extends React.Component<IObservationStationProfileProps, IObservationStationProfileState> {
+
+    constructor(props: IObservationStationProfileProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Card>
+                <h1>ObservationStationProfile</h1>
+            </Card>
+        );
+    }
+}
+
+interface IObservationStationProfileProps {
+    observationStation: ObservationStation;
+}
+
+interface IObservationStationProfileState { }
+
