6d2a8524b09f85b39e6d8cf2257efc01d6b8e213
ufoeu <jonas.zoll@student.kit.edu>
2020-07-08 20:26:01 +0200

    Add factory class for diagrams

---
+++ src/Controller/DiagramFactory.ts
@@ -0,0 +1,34 @@
+import Diagram from '../View/Diagram';
+import { IDiagramProps } from '../View/Diagram';
+import FeatureHistoryLineChart from '../View/FeatureHistoryLineChart';
+import YearComparisonLineChart from '../View/YearComparisonLineChart';
+import ComparisonToLastYearPieChart from '../View/ComparisonToLastYearPieChart';
+import { ObservationStation } from '../Model/ObservationStation';
+import { Feature } from '../Model/Feature';
+
+
+export default class DiagramFactory {
+
+    //Returns a diagram object of a type specified by the 'id' parameter
+    static getDiagram(id: string, observationStation: ObservationStation, feature: Feature) {
+        var diagram: Diagram;
+        var props: IDiagramProps = {
+            observationStation: observationStation,
+            feature: feature,
+        }
+
+        switch (id) {
+            case 'FeatureHistoryLineChart':
+                diagram = new FeatureHistoryLineChart(props);
+                break;
+            case 'YearComparisonLineChart':
+                diagram = new YearComparisonLineChart(props);
+                break;
+            case 'ComparisonToLastYearPieChart':
+                diagram = new ComparisonToLastYearPieChart(props);
+                break;
+            default:
+                throw new Error('Diagram type not supported!');
+        }
+    }
+}
