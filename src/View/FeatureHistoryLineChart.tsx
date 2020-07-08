fcc20b9a58683f57fdd47456938c5a52c7320196
ufoeu <jonas.zoll@student.kit.edu>
2020-07-08 20:24:39 +0200

    Add skeleton for FeatureHistoryLineChart

---
+++ src/View/FeatureHistoryLineChart.tsx
@@ -0,0 +1,16 @@
+import Diagram from './Diagram';
+import { IDiagramProps } from './Diagram';
+
+export default class FeatureHistoryLineChart extends Diagram {
+
+    constructor(props: IDiagramProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>FeatureHistoryLineChart</h1>
+        );
+    }
+
+}
