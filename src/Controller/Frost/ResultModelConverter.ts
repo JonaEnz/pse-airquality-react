9ee2855f734e6c504c2e6fa8e745ce3826e41d4d
ulfpr <ulfpr@student.kit.edu>
2020-07-14 20:10:31 +0200

    Add ResultModelConverter interface

--- src/Controller/Frost/ResultModelConverter.ts
+++ src/Controller/Frost/ResultModelConverter.ts
@@ -2,5 +2,6 @@ import { FrostResult } from "../../Model/FrostResult";
 
 export default interface ResultModelConverter<T> {
 
-    convert(json: any): FrostResult<T>;
+    convert(json: any): T;
+
 }
