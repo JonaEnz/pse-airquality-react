abf6ef78befff4e852da1bc66a0f307e04c30876
ufoeu <jonas.zoll@student.kit.edu>
2020-07-15 17:53:11 +0200

    Clean up and add comments

--- src/View/Diagrams/Diagram.tsx
+++ src/View/Diagrams/Diagram.tsx
@@ -1,102 +1,92 @@
 import React from 'react';
-import { Fragment } from 'react'
+import { Card, Divider, Typography, Grid, CircularProgress, Select, MenuItem, } from '@material-ui/core';
 import Chart from "react-google-charts";
 
-import { ObservationStation } from '../../Model/ObservationStation';
-import { Feature } from '../../Model/Feature';
-import DiagramController from '../../Controller/DiagramController';
-import {
-    CardContent,
-    Card,
-    Divider,
-    Typography,
-    Grid,
-    CircularProgress,
-    Select,
-    MenuItem,
-} from '@material-ui/core';
 import IDiagramController from '../../Controller/DiagramController';
 
 export default class Diagram extends React.Component<IDiagramProps, IDiagramState> {
+    //controller for this diagram
     controller: IDiagramController;
-    observationStation: ObservationStation;
-    feature: Feature;
-
-    configurationOptions: string[];
 
     constructor(props: IDiagramProps) {
         super(props);
         this.controller = this.props.controller;
-        this.observationStation = this.controller.observationStation;
-        this.feature = this.controller.feature;
-        this.configurationOptions = this.controller.getConfigurationOptions();
+
+        //setup default configuration in state
         this.state = {
             configurationOption: this.controller.getDefaultConfigurationOption(),
         }
+
         this.handleConfigurationChange = this.handleConfigurationChange.bind(this);
     }
 
+    //styles of this component
     styles = {
         header: {
             padding: '10px',
             paddingLeft: '40px',
         },
+        diagram: {
+            width: '100%',
+            height: '350px',
+        },
     };
 
+    //change configuration in state
     handleConfigurationChange(event: React.ChangeEvent<{ value: unknown }>) {
         this.setState({
             configurationOption: (event.target.value as string),
         });
     }
 
+    //return configuration options as menu items
     renderConfigurationOptions() {
+        var configurationOptions = this.controller.getConfigurationOptions();
         return (
-            this.configurationOptions.map((configurationOption) =>
-                <MenuItem value={configurationOption}>{configurationOption}</MenuItem>
+            configurationOptions.map((option) =>
+                <MenuItem value={option}>{option}</MenuItem>
             )
         );
     }
 
+    //render component
     render() {
         return (
-            <Fragment>
-                <Card>
-                    <div style={this.styles.header}>
-                        <Grid container justify='space-between' alignItems='center'>
-                            <Grid item>
-                                <Typography variant='subtitle1'>{this.feature.getName()}</Typography>
-                            </Grid>
-                            <Grid item>
-                                <Select
-                                    value={this.state.configurationOption}
-                                    onChange={this.handleConfigurationChange}
-                                >
-                                    {this.renderConfigurationOptions()}
-                                </Select>
-                            </Grid>
+            <Card>
+                <div style={this.styles.header}>
+                    <Grid container justify='space-between' alignItems='center'>
+                        <Grid item>
+                            <Typography variant='subtitle1'>{this.controller.feature.getName()}</Typography>
                         </Grid>
-                    </div>
-                    <Divider />
-                    <Chart
-                        width='100%'
-                        height='350px'
-                        chartType="LineChart"
-                        loader={
-                            <CardContent>
-                                <CircularProgress />
-                            </CardContent>
-                        }
-                        data={this.controller.getData(this.state.configurationOption)}
-                    />
-                </Card>
-
-            </Fragment>
+                        <Grid item>
+                            <Select
+                                value={this.state.configurationOption}
+                                onChange={this.handleConfigurationChange}
+                            >
+                                {this.renderConfigurationOptions()}
+                            </Select>
+                        </Grid>
+                    </Grid>
+                </div>
+                <Divider />
+                <Chart
+                    width={this.styles.diagram.width}
+                    height={this.styles.diagram.height}
+                    chartType={this.controller.getChartType()}
+                    loader={
+                        <Grid container justify='center' alignContent='center' style={this.styles.diagram}>
+                            <CircularProgress />
+                        </Grid>
+                    }
+                    data={this.controller.getData(this.state.configurationOption)}
+                />
+            </Card>
         );
     }
 }
 
 export interface IDiagramProps {
-    controller: DiagramController;
+    controller: IDiagramController;
 }
 
 interface IDiagramState {
