10799e0220de9bb9f98080cf2155ee86221ed91b
ufoeu <jonas.zoll@student.kit.edu>
2020-07-20 15:48:04 +0200

    Add new diagrams to diagram factory

--- src/Controller/DiagramFactory.ts
+++ src/Controller/DiagramFactory.ts
@@ -1,11 +1,15 @@
 import { ObservationStation } from '../Model/ObservationStation';
 import { Feature } from '../Model/Feature';
 import FeatureHistoryLineChartController from './FeatureHistoryLineChartController';
+import ComparisonToLastYearPieChartController from './ComparisonToLastYearPieChartController';
+import YearComparisonLineChartController from './YearComparisonLineChartController';
 import IDiagramController from './DiagramController';
 
 export default class DiagramFactory {
     private static readonly diagramMap = new Map<string, Function>([
         ['FeatureHistoryLineChart', (observationStation: ObservationStation, feature: Feature) => { return new FeatureHistoryLineChartController(observationStation, feature) }],
+        ['ComparisonToLastYearPieChart', (observationStation: ObservationStation, feature: Feature) => { return new ComparisonToLastYearPieChartController(observationStation, feature) }],
+        ['YearComparisonLineChart', (observationStation: ObservationStation, feature: Feature) => { return new YearComparisonLineChartController(observationStation, feature) }],
     ]);
 
     public static getDiagramController(id: string, observationStation: ObservationStation, feature: Feature): IDiagramController {
