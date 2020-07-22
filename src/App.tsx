9020634f9c5dbbbc6eafad5e7b4233a40210603d
ufoeu <jonas.zoll@student.kit.edu>
2020-07-22 12:17:09 +0200

    Remove unused files and implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import React from "react";
 import Layout from "./View/Layout";
 
-import "./App.css";
-import DetailPage from './View/DetailPage';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { green } from '@material-ui/core/colors'
-
 function App() {
-
-  const theme = createMuiTheme({
-    palette: {
-      primary: green,
-    }
-  });
-
   return (
     <div className="App">
       <Layout />
