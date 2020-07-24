bfa586ead003dd5f84a1ad77b9572a0c2f6872f5
Anna Csurko <pankarinko@gmail.com>
2020-07-24 17:11:34 +0200

    Insert text in About page

--- src/View/AboutPage.tsx
+++ src/View/AboutPage.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Fragment } from "react";
+import Language from "../Controller/Storage/Language";
+
+let language = Language.getInstance();
 
 export default class AboutPage extends React.Component<
     IAboutPageProps,
     IAboutPageState
-> {
+    >{
+
     render() {
         return (
             <Fragment>
-                <h1>AboutPage</h1>
+                <h1>{language.getText("about")}</h1>
+                <h3>{language.getText("aboutText")}</h3>
             </Fragment>
         );
     }
 }
 
-interface IAboutPageProps {}
+interface IAboutPageProps { }
 
-interface IAboutPageState {}
+interface IAboutPageState { }
