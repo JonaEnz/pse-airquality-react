d4cedba280e6bb8dadb79bf5ed19a1dedc922383
ulfpr <ulfpr@student.kit.edu>
2020-07-25 19:06:09 +0200

    Complete GetStation (now working except Features)

--- src/Controller/Frost/getStation/GetStation.ts
+++ src/Controller/Frost/getStation/GetStation.ts
@@ -11,11 +11,17 @@ export class GetStationFactory extends FrostFactory<ObservationStation> {
 }
 
 export class GetStationConverter implements ResultModelConverter<ObservationStation> {
-    public convert(json: any): ObservationStation {
-        //TODO
+    public convert(json: ObservationStationEntity): ObservationStation {
+        let pos: Position;
+        if (json.Locations === undefined || json.Locations === null) {
+            pos = new Position(0, 0);
+        } else {
+            pos = new Position(json.Locations[0].location.coordinates[0], json.Locations[0].location.coordinates[1]);
+        }
 
 
-        return new ObservationStation("", "", "", new Position(0, 0), []);
+
+        return new ObservationStation(json["@iot.id"], json.name, json.description, pos, []);
     }
 }
 
@@ -23,10 +29,33 @@ export class GetStationConverter implements ResultModelConverter<ObservationStat
 export class GetStationBuilder implements QueryBuilder {
 
     public getQuery(options: getStationOptions): string {
-        return "Things('" + options.id + "')?$expand=Locations";
+        return "Things('" + options.id + "')?$select=@iot.id,name,description&$expand=Locations($select=location),Datastreams/ObservedProperty($select=@iot.id)";
     }
 }
 
 export interface getStationOptions {
     id: string;
+}
+
+
+interface ObservationStationEntity {
+    name: string;
+    description: string;
+    Datastreams?: (DatastreamsEntity)[] | null;
+    Locations?: (LocationsEntity)[] | null;
+    "@iot.id": string;
+}
+interface DatastreamsEntity {
+    name: string;
+    ObservedProperty: ObservedPropertyEntity;
+}
+interface ObservedPropertyEntity {
+    "@iot.id": string;
+}
+interface LocationsEntity {
+    location: LocationEntity;
+}
+interface LocationEntity {
+    type: string;
+    coordinates: (number)[];
 }
