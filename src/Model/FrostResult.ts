c666a54b44c4acf4b71e3b2381bb949e5bc5152d
ulfpr <ulfpr@student.kit.edu>
2020-07-25 19:06:21 +0200

    Spacing

--- src/Model/FrostResult.ts
+++ src/Model/FrostResult.ts
@@ -1,31 +1,34 @@
 export class FrostResult<T> {
-  private result: T | null;
-  private success: boolean;
-  private errorMessage: string;
+    private result: T | null;
+    private success: boolean;
+    private errorMessage: string;
 
-  constructor(result: T | null, success: boolean, errorMessage: string) {
-    if (success || !result) {
-      //object is not null or success
-      this.result = result;
-      this.success = true;
-      this.errorMessage = "";
-    } else {
-      //object is null and no success
-      this.result = null;
-      this.success = false;
-      this.errorMessage = errorMessage;
+    constructor(result: T | null, success: boolean, errorMessage: string) {
+        if (success || result !== null || result !== undefined) {
+            //object is not null or success
+            this.result = result;
+            this.success = true;
+            this.errorMessage = "";
+        } else {
+            //object is null and no success
+            this.result = null;
+            this.success = false;
+            this.errorMessage = errorMessage;
+        }
     }
-  }
 
-  getResult(): T | null {
-    return this.result;
-  }
+    getResult(): T | null {
+        if (this.result === undefined) {
+            return null;
+        }
+        return this.result;
+    }
 
-  getSuccess(): boolean {
-    return this.success;
-  }
+    getSuccess(): boolean {
+        return this.success;
+    }
 
-  getMessage(): string {
-    return this.errorMessage;
-  }
+    getMessage(): string {
+        return this.errorMessage;
+    }
 }
