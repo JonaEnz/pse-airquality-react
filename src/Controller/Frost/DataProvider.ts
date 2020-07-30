9f8c689e1b85c4d8b09efd988b07c66417ca77c6
ulfpr <ulfpr@student.kit.edu>
2020-07-30 19:04:36 +0200

    Modify

--- src/Controller/Frost/DataProvider.ts
+++ src/Controller/Frost/DataProvider.ts
@@ -10,35 +10,24 @@ import { GetStationFactory } from "../Frost/factories/GetStation";
 import { FrostResult } from "../../Model/FrostResult";
 import { GetObservationStationsFactory } from "../Frost/factories/GetObservationStations";
 import { GetLatestObservationFactory } from "../Frost/factories/GetLatestObservation";
+import { GetLatestObservationsFactory } from "./factories/GetLatestObservations";
 
 export default class DataProvider {
 
-    private static instance: DataProvider;
+    private static server: FrostServer = new FrostServer("https://api.smartaq.net/v1.0/");
 
-    private server: FrostServer;
-
-    constructor() {
-        this.server = new FrostServer("https://api.smartaq.net/v1.0/");
-    }
-
-
-    static getInstance(): DataProvider {
-        if (this.instance === null) {
-            this.instance = new DataProvider();
-        }
-        return this.instance;
-    }
-
-    async getObservationStations(middle: Position, radius: number): Promise<ObservationStation[]> {
+    static async getObservationStations(middle: Position, radius: number): Promise<ObservationStation[]> {
         let fr: FrostResult<ObservationStation[]> = await this.server.request(new GetObservationStationsFactory(), { middle, radius });
         let obsnull: ObservationStation[] | null = fr.getResult();
         if (obsnull !== null) {
             return obsnull;
         }
-        throw new Error("nö");
+        alert("dp error");
+        alert(fr.getMessage());
+        throw new Error(fr.getMessage());
     }
 
-    async getLatestObservation(station: ObservationStation, feature: Feature): Promise<Observation> {
+    static async getLatestObservation(station: ObservationStation, feature: Feature): Promise<Observation> {
         let fr: FrostResult<Observation> = await this.server.request(new GetLatestObservationFactory(), { station, feature });
         let obsnull: Observation | null = fr.getResult();
         if (obsnull !== null) {
@@ -47,12 +36,23 @@ export default class DataProvider {
         throw new Error("nö");
     }
 
-    async getStation(id: string): Promise<ObservationStation> {
+    static async getStation(id: string): Promise<ObservationStation> {
         let fr: FrostResult<ObservationStation> = await this.server.request(new GetStationFactory(), { id });
         let obsnull: ObservationStation | null = fr.getResult();
         if (obsnull !== null) {
             return obsnull;
         }
+        alert("dp error");
         throw new Error("nö");
+    }
+
+    static async getLatestObservations(center: Position, radius: number, feature: Feature): Promise<Observation[]> {
+        let fr: FrostResult<Observation[]> = await this.server.request(new GetLatestObservationsFactory(), { center, radius, feature });
+        let obsnull: Observation[] | null = fr.getResult();
+        if (obsnull !== null) {
+            return obsnull;
+        }
+        alert(fr.getMessage() + "dp spec");
+        return [];
     }
 }
