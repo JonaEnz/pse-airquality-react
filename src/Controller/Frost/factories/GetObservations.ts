f92a329cf23e0c61e271f79cd72f43b2479ba8d0
ulfpr <ulfpr@student.kit.edu>
2020-07-30 21:06:53 +0200

    Implement GetObservations

--- src/Controller/Frost/factories/GetObservations.ts
+++ src/Controller/Frost/factories/GetObservations.ts
@@ -2,7 +2,6 @@ import QueryBuilder from '../QueryBuilder';
 import FrostFactory from '../FrostFactory';
 import { Observation } from '../../../Model/Observation';
 import ResultModelConverter from '../ResultModelConverter';
-import { Position } from '../../../Model/Position';
 import { ObservationStation } from '../../../Model/ObservationStation';
 import { Feature } from '../../../Model/Feature';
 
@@ -13,8 +12,22 @@ export class GetObservationsFactory extends FrostFactory<Observation[]> {
 }
 
 export class GetObservationsConverter implements ResultModelConverter<Observation[]> {
-    public convert(json: any, options: GetObservationsOptions): Observation[] {
-        return [];
+    public convert(json: ResultList, options: GetObservationsOptions): Observation[] {
+        if (json.value === null || json.value === undefined) {
+            throw new Error("nö");
+        }
+        let observations: Observation[] = [];
+        json.value.forEach(stream => {
+            if (stream.Observations === null || stream.Observations === undefined) {
+                return;
+            }
+            stream.Observations.forEach(obs => {
+                if (obs !== null) {
+                    observations.push(new Observation(options.station, options.feature, obs.result, new Date(obs.phenomenonTime)));
+                }
+            });
+        });
+        return observations;
     }
 }
 
@@ -32,3 +45,24 @@ export interface GetObservationsOptions {
     start: Date;
     end: Date;
 }
+
+export interface ResultList {
+    value?: (ValueEntity)[] | null;
+}
+export interface ValueEntity {
+    Observations?: (ObservationsEntity | null)[] | null;
+    "@iot.id": string;
+    "Observations@iot.nextLink"?: string | null;
+}
+export interface ObservationsEntity {
+    phenomenonTime: string;
+    resultTime: string;
+    result: number;
+    parameters: Parameters;
+    "@iot.id": string;
+    "@iot.selfLink": string;
+}
+export interface Parameters {
+    "last calibration": string;
+}
+
