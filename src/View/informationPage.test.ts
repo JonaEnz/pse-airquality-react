e49ce16757b6e4650a229fe7888123cfce1ac406
Anna Csurko <pankarinko@gmail.com>
2020-08-08 23:52:05 +0200

    Re-insert diagram tests

---
+++ src/View/informationPage.test.ts
@@ -0,0 +1,10 @@
+import React from "react";
+import Language from "../Controller/Storage/Language";
+import "./informationPage.css";
+import AboutPage from "./About";
+import PirvacyPolicy from "./PrivacyPolicy";
+import ErrorPage from "./ErrorPage";
+
+let language = Language.getInstance();
+
+let about = AboutPage;
