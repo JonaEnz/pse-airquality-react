738e0791c6bbb19aa26b2527479c7b2243e65778
Anna Csurko <pankarinko@gmail.com>
2020-08-22 20:39:36 +0200

    Add Diagra.tsx to diagram test

--- src/tests/Controller/Diagram.test.ts
+++ src/tests/Controller/Diagram.test.ts
@@ -7,6 +7,7 @@ import { ObservationStation } from "../../Model/ObservationStation";
 import { Scale } from "../../Model/Scale";
 import { Position } from "../../Model/Position";
 import Language from "../../Controller/Storage/Language";
+import Diagram from "../../View/DetailPage/Diagram";
 
 let feature = new Feature(
     "1",
@@ -24,6 +25,8 @@ let feature = new Feature(
     ["1", "2"],
     "iconName"
 );
+
+let diagram = new Diagram(FeatureHistoryLineChartController);
 
 let station = new ObservationStation(
     "0001",
