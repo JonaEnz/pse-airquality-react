5d84939adce744205859c942c0e10a7bddc170a9
ulfpr <ulfpr@student.kit.edu>
2020-09-02 22:28:46 +0200

    Add tests for FrostServer

--- src/tests/Controller/Frost/FrostServer.test.tsx
+++ src/tests/Controller/Frost/FrostServer.test.tsx
@@ -5,7 +5,19 @@ import { FrostResult } from "../../../Model/FrostResult";
 
 class TestStationFactory extends FrostFactory<number> {
     constructor() {
-        super({ convert: jest.fn((json, options) => json.testNumber) }, { getQuery: jest.fn(() => "testQuery") });
+        super(
+            {
+                convert: jest.fn((json, options) => {
+                    if (json.attribute === testJson.attribute) {
+                        return 8;
+                    } else {
+                        throw new Error();
+                    }
+                })
+            },
+            {
+                getQuery: jest.fn(() => "testQuery")
+            });
     }
 }
 
@@ -21,3 +33,28 @@ test("test for url handling", () => {
     fs.setUrl(url);
     expect(fs.getUrl()).toBe(url);
 });
+
+
+test("test request method", () => {
+    expect.assertions(1);
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve(
+        { json: () => Promise.resolve(testJson), ok: true }));
+    return fs.request(new TestStationFactory(), {}).then((res) => expect(res.getResult()).toEqual(8));
+});
+
+test("test request method with error in convert", () => {
+    expect.assertions(1);
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve(
+        { json: () => Promise.resolve(faultyJson), ok: true }));
+    return fs.request(new TestStationFactory(), {}).then((res) => expect(res.getSuccess()).toBeFalsy());
+});
+
+test("test request method with fetch error", () => {
+    expect.assertions(1);
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve(
+        { ok: false }));
+    return fs.request(new TestStationFactory(), {}).then((res) => expect(res.getSuccess()).toBeFalsy());
+});
+
+const testJson = { attribute: "string" };
+const faultyJson = { attribut: "meh" };
