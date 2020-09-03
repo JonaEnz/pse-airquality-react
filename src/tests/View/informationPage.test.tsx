f7d8044b75669926702feb68a48fe41adf3d2803
Anna Csurko <pankarinko@gmail.com>
2020-09-03 16:48:28 +0200

    Add test for HowItWorks rende

--- src/tests/View/informationPage.test.tsx
+++ src/tests/View/informationPage.test.tsx
@@ -3,6 +3,7 @@ import Language from "../../Controller/Storage/Language";
 import "../../View/informationPage.css";
 import AboutPage from "../../View//About";
 import ErrorPage from "../../View//ErrorPage";
+import HowItWorks from "../../View/HowItWorks";
 import { shallow, mount, render } from 'enzyme';
 import PrivacyPolicyPage from "../../View/PrivacyPolicy";
 
@@ -21,5 +22,10 @@ test("PrivcyolicyPage renders", () => {
 
 test("ErrorPage renders", () => {
     let wrapped = shallow(<ErrorPage code={404} message={"Page not found"} />);
+    expect(wrapped.exists()).toBe(true);
+});
+
+test("Instruction renders", () => {
+    let wrapped = shallow(<HowItWorks />);
     expect(wrapped.exists()).toBe(true);
 });
